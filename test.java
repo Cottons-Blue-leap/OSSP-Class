0f0ab9f960b575075c85686d884b6e9498fc91ec
Tae-kyu <don9292@dongguk.edu>
2022-03-31 13:13:33 +0900

    hello Wolrd 5 times

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        for (int i = 0; i<5; i++) {
+		 System.out.println("hello world");
+	}
     }
 }
